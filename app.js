aa5fbba3eab2d3ba8c89eb6edfe9209156772241
Tsuneki Umeda <umedat@amazon.co.jp>
2020-12-23 18:41:42 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes')
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: ' Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.body)
+  },
+})
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a new note',
+  handler: function () {
+    console.log('Removing a new note!')
+  },
+})
+
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing out all note')
+  },
+})
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a  note',
+  handler: function () {
+    console.log('Reading a note')
+  },
+})
+
+yargs.parse()
+// console.log(yargs.argv)
