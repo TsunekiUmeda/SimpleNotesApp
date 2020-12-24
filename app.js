a711b0bf6bec08909eef94455a6634db35efd309
Tsuneki Umeda <umedat@amazon.co.jp>
2020-12-24 16:57:29 +0900

    removing Note

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const notes = require('./notes')
 
-
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
@@ -26,8 +25,15 @@ yargs.command({
 yargs.command({
   command: 'remove',
   describe: 'Remove a new note',
-  handler: function () {
-    console.log('Removing a new note!')
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    notes.removeNotes(argv.title)
   },
 })
 
--- notes.js
+++ notes.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk')
 const fs = require('fs')
 const getNotes = function () {
   return 'Your Notes...'
@@ -17,15 +18,32 @@ const addNotes = function (title, body) {
     })
 
     saveNotes(notes)
-    console.log('New note added!')
+    console.log(chalk.green.inverse('New Note added'))
   } else {
-    console.log('Note title taken!')
+    console.log(chalk.red.inverse('Note title taken'))
   }
 }
 
 const saveNotes = function (notes) {
   const dataJSON = JSON.stringify(notes)
   fs.writeFileSync('notes.json', dataJSON)
+}
+
+const removeNotes = function (title) {
+  const notes = loadNotes()
+
+  const notesToKeep = notes.filter(function (note) {
+    return note.title !== title
+  })
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse('Note removed'))
+    saveNotes(notesToKeep)
+  } else {
+    console.log(chalk.red.inverse('No Note found'))
+  }
+
+  console.log('Note removed!')
 }
 
 const loadNotes = function () {
@@ -41,4 +59,5 @@ const loadNotes = function () {
 module.exports = {
   getNotes: getNotes,
   addNotes: addNotes,
+  removeNotes: removeNotes,
 }
